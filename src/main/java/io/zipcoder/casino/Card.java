cbd7e536f5c8cfc6aa73e1a19a588e101599953a
Kris Blassingame <krisbl@zipcoders-MacBook-Pro-4.local>
2018-06-07 15:31:27 -0400

    finished deck and card classes

--- src/main/java/io/zipcoder/casino/Card.java
+++ src/main/java/io/zipcoder/casino/Card.java
@@ -1,21 +1,60 @@
 package io.zipcoder.casino;
 
 public class Card {
-    Suit aSuit;
-    Rank aRank;
 
-    public void setSuit(){
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
     }
 
-    public Suit getSuit(){
-        return Suit.SPADES;
+    public Suit getSuit() {
+        return suit;
     }
 
-    public void setRank(){
+
+    public Rank getRank() {
+        return rank;
     }
 
-    public Rank getRank(){
-        return Rank.ACE;
-    }
 
 }
+
+
+
+
+ /*   int n = Suit * Rank;
+    String[] deck = new String[n];
+        for (int i = 0; i < Rank.length; i++) {
+        for (int j = 0; j < SUITS.length; j++) {
+            deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
+        }
+    }*/
+
+
+//    Suit aSuit;
+//    Rank aRank;
+//
+//    public void setSuit(){
+//    }
+//
+//    public static Suit getSuit(){
+//       return Suit.SPADES;
+//
+//    }
+//
+//    public void setRank(){
+//    }
+//
+//    public static Rank getRank(){
+//        return Rank.ACE;
+//    }
+//
+//    public Card(int Suit, int Rank) {
+//        this.aSuit = aSuit;
+//        this.aRank = aRank;
+//    }
+
+
--- src/main/java/io/zipcoder/casino/Deck.java
+++ src/main/java/io/zipcoder/casino/Deck.java
@@ -1,11 +1,45 @@
 package io.zipcoder.casino;
 
+import java.util.*;
+
 public class Deck {
-    Card[] setOfCard = new Card[52];
 
-    public void drawCard(){
+    private ArrayList deck;
+
+    public Deck() {
+        this.deck = new ArrayList();
+        for (Rank rank : Rank.values()) {
+            for (Suit suit : Suit.values()) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+
+        Collections.shuffle(deck);
+
+        Iterator cardIterator = deck.iterator();
+        while (cardIterator.hasNext()) {
+            Card card = (Card) cardIterator.next();
+            System.out.println(card.getRank() + " of " + card.getSuit());
+        }
+
     }
 
-    public void shuffle(){
-    }
+
 }
+
+
+//    Deck[] deck = new Deck[52];
+//
+//
+//
+//
+//
+//
+//
+//    public String drawCard() {
+//        return Card.getRank() + "of " + Card.getSuit();
+//    }
+//
+//    public void shuffle() {
+//    }
+
