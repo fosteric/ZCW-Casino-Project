eca68c2fc751c3e5f7521bc2fb8da7aaf27f868a
Chad <Hallinanchad@gmail.com>
2018-06-06 20:04:59 -0400

    Working dice/tests!

---
+++ src/test/java/io/zipcoder/casino/DiceTest.java
@@ -0,0 +1,52 @@
+package io.zipcoder.casino;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+    /**
+     * The test class DiceTest.
+
+     */
+    public class DiceTest
+    {
+        Dice test = new Dice();
+        /**
+         * Default constructor for test class DiceTest
+         */
+        public DiceTest()
+        {
+        }
+
+        /**
+         * Sets up the test fixture.
+         *
+         * Called before every test case method.
+         */
+        @Before
+        public void setUp()
+        {
+        }
+
+        @Test
+        public void rollTest(){
+            //Given
+            int roll = 5;
+            //When
+            int actual = 5;
+            int expected = test.roll(roll);
+            //Result
+            assertEquals(actual, expected, 20);
+        }
+
+        /**
+         * Tears down the test fixture.
+         *
+         * Called after every test case method.
+         */
+        @After
+        public void tearDown()
+        {
+        }
+    }
+
