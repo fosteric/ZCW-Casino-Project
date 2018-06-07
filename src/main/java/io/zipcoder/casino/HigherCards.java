651f5fc51516fe36bf8e79e68460aa4f77d74ba1
Aleena Rose-Mathew <AleenaRM@zipcoders-MacBook-Pro-81.local>
2018-06-07 14:21:55 -0400

    first commit

--- src/main/java/io/zipcoder/casino/HigherCards.java
+++ src/main/java/io/zipcoder/casino/HigherCards.java
@@ -2,8 +2,22 @@ package io.zipcoder.casino;
 
 public class  HigherCards extends CardGame implements Gambling{
 
-    public boolean playGame(Player aPlayer) {
-        return false;
+     public boolean playGame(Player aPlayer) {
+        Boolean result;
+        int player=draw();
+        int croupier=draw();
+        if(player>croupier)
+        {
+            result=true;
+            aPlayer.totalChips=aPlayer.getTotalChips()+bet;
+        }
+        else
+        {
+            result=false;
+            aPlayer.totalChips=aPlayer.getTotalChips()-bet;
+        }
+
+        return result;
     }
 
     public int wageMoney() {
