54f22b7285282b903e193432b05787fb0a4f3082
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-06-07 17:34:36 -0400

    yahtzee additional changes

--- src/main/java/io/zipcoder/casino/Yahtzee.java
+++ src/main/java/io/zipcoder/casino/Yahtzee.java
@@ -1,34 +1,58 @@
 package io.zipcoder.casino;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Yahtzee extends DiceGame {
 
     Console console = new Console();
+    Map<YahtzeeFields, Integer> scoreSheet = new HashMap<YahtzeeFields, Integer>();
     Dice dice = new Dice();
-    Die[] currentDiceRoll;
+    Die[] diceRoll;
 
     public boolean playGame(Player aPlayer) {
         //roll dice (1)
             //roll all 5 dice
-            currentDiceRoll = dice.roll(5);
+        //See current dice
         //Use roll or roll again?
             //If roll again, select which dice to keep
                 //roll remaining dice (2)
-            ArrayList<Dice> additionalDiceRoll1 =
             //If use roll, select a field
+        //See current dice
         //Use roll or roll again?
             //If roll again, select which dice to keep
                 //roll remaining dice (3)
             //If use roll, select a field
+        //See current dice
         //Select a field
+
+        //other things you might want to do..
+            //See fields and scoring formula
+            //see current score sheet
+            //quit game
+
+
         return false;
     }
 
-    public Die[] rollAgain(int numberOfDice){
-        console.
-        Die[] additionalDiceRoll;
-        additionalDiceRoll = dice.roll(numberOfDice);
+    public Die[] rollAllDice(){
+        //roll and return the entire array of dice
+        return null;
+    }
 
+    public void rollAgain(int... diePosition){
+        //roll specific die in the dice array
+    }
+
+    public int scoreRoll(YahtzeeFields yahtzeeField){
+        //take in field and dice array and give a score
+        return -1;
+    }
+
+    public void updateScoreSheet(YahtzeeFields yahtzeeField, int score){
+        //Take in field and score and update sheet
+    }
+
+    public void displayScoreSheet(){
+        //print score sheet
     }
 
 }
---
+++ src/main/java/io/zipcoder/casino/YahtzeeFields.java
@@ -0,0 +1,16 @@
+package io.zipcoder.casino;
+
+public enum YahtzeeFields {
+    ACES,
+    TWOS,
+    THREES,
+    FOURS,
+    FIVES,
+    SIXES,
+    THREEOFAKIND,
+    FOUROFAKIND,
+    FULLHOUSE,
+    SMSTRAIGHT,
+    LGSTRAIGHT,
+    YAHTZEE;
+}
