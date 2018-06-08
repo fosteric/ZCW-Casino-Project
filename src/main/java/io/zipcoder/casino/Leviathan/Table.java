71ece7183d2ef9437d1a1541a9912750b1643904
Chad <Hallinanchad@gmail.com>
2018-06-08 18:08:32 -0400

    Making a working table

--- src/main/java/io/zipcoder/casino/Leviathan/Table.java
+++ src/main/java/io/zipcoder/casino/Leviathan/Table.java
@@ -9,52 +9,44 @@ public class Table {
     Game aGame;
     Player aPlayer;
 
-    public Table(Player aPlayer){
+    public Table(Player aPlayer) {
         this.aPlayer = aPlayer;
     }
 
-    public void startGame(){
-        Boolean result=false;
+    public void startGame() {
+        Boolean result = false;
         aConsole.println("Welcome to Casino");
         aConsole.println("Choose the game");
         aConsole.println("1.Higher Dice\n2.Higher Card\n3.Black Jack\n4.Yahtzee\n\n");
 
-        int choice=aConsole.getIntInput("Enter your choice");
-        if(choice==1)//Higher Dice
+        int choice = aConsole.getIntInput("Enter your choice");
+        if (choice == 1)//Higher Dice
         {
-            HigherDice higherDice=new HigherDice (aPlayer);
+            HigherDice higherDice = new HigherDice(aPlayer);
             higherDice.playGame();
-        }
-        else if(choice==2)//Higher Cards
+        } else if (choice == 2)//Higher Cards
         {
-            HigherCards higherCards=new HigherCards(aPlayer);
+            HigherCards higherCards = new HigherCards(aPlayer);
             higherCards.playGame();
-        }
-        else if(choice==3)
-        {
-            //BlackJack blackJack=new BlackJack(aPlayer);
-            BlackJack blackJack=new BlackJack();
+        } else if (choice == 3) {
+            BlackJack blackJack=new BlackJack(aPlayer);
             blackJack.playGame();
-        }
-        else if(choice==4)
-        {
-            Yahtzee yahtzee =new Yahtzee(aPlayer);
+        } else if (choice == 4) {
+            Yahtzee yahtzee = new Yahtzee(aPlayer);
             yahtzee.playGame();
         }
-        if(result.equals(true))
-        {
+        if (result.equals(true)) {
             aConsole.println("Congrats! You Won");
             aConsole.println("Your current available Chips");
-            String availableChips= aPlayer.getTotalChips().toString();
+            String availableChips = aPlayer.getTotalChips().toString();
             aConsole.println(availableChips);
-        }
-        else
-        {
+        } else {
             aConsole.println("You Lose");
 
         }
 
     }
+}
 
 
 
