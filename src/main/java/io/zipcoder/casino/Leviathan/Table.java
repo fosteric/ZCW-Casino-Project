158e5c1e900abbb185319dcf52321ef1d223f9f8
Chad <Hallinanchad@gmail.com>
2018-06-09 15:00:38 -0400

    yes or no console added, began working on Dice Builder

--- src/main/java/io/zipcoder/casino/Leviathan/Table.java
+++ src/main/java/io/zipcoder/casino/Leviathan/Table.java
@@ -54,7 +54,7 @@ public class Table {
                 aGame = new BlackJack(aPlayer);
                 aGame.playGame();
                 played = true;
-                aConsole.println("You Lose");
+                aConsole.println("You Lose\n");
             } else if (choice == 4) //Yahtzee
             {
                 aGame = new Yahtzee(aPlayer);
