c27ca734902113c0b4d108cd88b0427c60440441
Aleena Rose-Mathew <AleenaRM@zipcoders-MacBook-Pro-81.local>
2018-06-09 15:33:26 -0400

    almost done

--- src/main/java/io/zipcoder/casino/Leviathan/Games/HigherCards.java
+++ src/main/java/io/zipcoder/casino/Leviathan/Games/HigherCards.java
@@ -9,8 +9,6 @@ import io.zipcoder.casino.Leviathan.*;
 
 public class  HigherCards extends CardGame implements Gambling{
     Console aConsole=new Console();
-    int wageAmount;
-    int totalChips;
     Player aPlayer;
     boolean playAgain = true;
 
@@ -19,9 +17,15 @@ public class  HigherCards extends CardGame implements Gambling{
     }
 
      public void playGame() {
-
-         Boolean result;
-         while (playAgain == true) {
+         aConsole.print("Welcome to HigherCards!\nWe both will draw a card, and the higher card wins the wager.\nThe House wins on ties\n");
+         while (playAgain) {
+             /*aConsole.println("\u2660");//spades
+             aConsole.println("\u2665");//heart
+             aConsole.println("\u2666");//diamond
+             aConsole.println("\u2663");//suit
+             String heart="\u2660";
+             int k=6;
+             aConsole.println(heart);*/
              int wageAmount = wageMoney();
 
              Deck deck = new Deck();
@@ -30,7 +34,10 @@ public class  HigherCards extends CardGame implements Gambling{
              Rank rank = acard.getRank();
              int player = rank.getValue();
              String playervalueString=rank.toString();
+             String playerFace=rank.getFaceValue();
+
              Suit suit = acard.getSuit();
+             String suitSymbol=suit.getsuitSymbol();
              String suitString = suit.toString();
 
              aConsole.println("You got :%s of %s",playervalueString,suitString);
@@ -39,24 +46,29 @@ public class  HigherCards extends CardGame implements Gambling{
              aConsole.println("\nYour rank is :");
              String rankValue = rank.toString();
              aConsole.println(rankValue);*/
+             printCard(suitSymbol,playerFace);
 
              Card acard2 = deck.draw();
              Rank rank2 = acard2.getRank();
+             int croupier = rank2.getValue();
              String playervalueString2=rank2.toString();
+             String playerFace2=rank2.getFaceValue();
+
              Suit suit2 = acard2.getSuit();
              String suitString2 = suit2.toString();
-             int croupier = rank2.getValue();
+             String suitSymbol2=suit2.getsuitSymbol();
              aConsole.println("House got :%s of %s",playervalueString2,suitString2);
              /*aConsole.println("\n\nHouse suit is :");
              aConsole.print(suitString2);
              aConsole.println("\nHouse rank is :");
              String rankValue2 = rank2.toString();
              aConsole.println(rankValue2);*/
+             printCard(suitSymbol2,playerFace2);
 
              int totalChips = aPlayer.getTotalChips();
-             int newTotalChips = findWinner(player, croupier, wageAmount, totalChips);
-             if (newTotalChips > totalChips) {
-                 aConsole.println("Congrats ! You Won");
+             findWinner(player, croupier, wageAmount);
+             if (aPlayer.getTotalChips() > totalChips) {
+                 aConsole.println("Congrats ! You Won ");
                  aConsole.println("Your current available Chips");
                  String availableChips = aPlayer.getTotalChips().toString();
                  aConsole.println(availableChips);
@@ -75,19 +87,19 @@ public class  HigherCards extends CardGame implements Gambling{
 
          }
      }
-    public int findWinner(int player,int croupier,int wageAmount,int totalChips)
+    public void findWinner(int player,int croupier,int wageAmount)
     {
 
         if (player > croupier) {
 
-           totalChips = totalChips+ wageAmount;
+           aPlayer.setTotalChips(aPlayer.getTotalChips()+ wageAmount);
         }
         else {
 
-            totalChips = totalChips-wageAmount;
+            aPlayer.setTotalChips(aPlayer.getTotalChips()- wageAmount);
         }
-        aPlayer.setTotalChips(totalChips);
-        return totalChips;
+
+
     }
 
 
@@ -102,5 +114,19 @@ public class  HigherCards extends CardGame implements Gambling{
 
         return bet;
     }
+
+    public void printCard(String suitSymbol,String playerFace)
+    {
+        aConsole.println(" ---------");
+        aConsole.println("| %s     %s |",playerFace,suitSymbol);
+        for(int i=0;i<5;i++)
+        {
+            aConsole.println("|         |");
+        }
+        aConsole.println("| %s     %s |",suitSymbol,playerFace);
+        aConsole.println(" ---------");
+    }
+
+
 }
 
